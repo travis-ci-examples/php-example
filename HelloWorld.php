01978cde1ed16aa15f1bec2b27a34d462b53645a
lsmith77 <smith@pooteeweet.org>
2011-12-20 23:43:11 +0100

    added DB env example

--- HelloWorld.php
+++ HelloWorld.php
@@ -2,8 +2,28 @@
 
 class HelloWorld
 {
+    /**
+     * @var PDO
+     */
+    private $pdo;
+
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
     public function hello($what = 'World')
     {
+        $sql = "INSERT INTO hello_world_test.hello VALUES (" . $this->pdo->quote($what) . ")";
+        $this->pdo->query($sql);
         return "Hello $what";
+    }
+
+
+    public function what()
+    {
+        $sql = "SELECT what FROM hello_world_test.hello";
+        $stmt = $this->pdo->query($sql);
+        return $stmt->fetchColumn();
     }
 }
