a83d66a54a180c2601a515f6979585a88da7fa7f
Arthur SCHMITT <aschmitt@inbox.fr>
2018-05-04 11:09:06 +0200

    Add exclamation mark for testing the travis build

--- HelloWorld.php
+++ HelloWorld.php
@@ -12,7 +12,7 @@ class HelloWorld
         $this->pdo = $pdo;
     }
 
-    public function hello($what = 'World')
+    public function hello($what = 'World !')
     {
         $sql = "INSERT INTO hello VALUES (" . $this->pdo->quote($what) . ")";
         $this->pdo->query($sql);
